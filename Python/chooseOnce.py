da835f56b1a15ad7e8302dbe4355344f1e53eeb0
luckyScript <jsjhlk@163.com>
2015-11-11 17:49:15 +0800

    fix bugs

--- Python/chooseOnce.py
+++ Python/chooseOnce.py
@@ -1,15 +1,14 @@
 # author kay
 # Find the element that appears once
 
-# Given an array where every element occurs three times, except one element which occurs only once. Find the element that occurs once. Eipected time compleiity is O(n) and O(1) eitra space.
+# Given an array where every element occurs three times, except one element which occurs only once. Find the element that occurs once. Expected time complexity is O(n) and O(1) extra space.
 # Examples:
 
 # Input: arr[] = {12, 1, 12, 3, 12, 1, 1, 2, 3, 3}
 # Output: 2
 def chooseOnce(arr):
-	once, twice = 0, 0
-	for i in arr:
-		once  = (once ^ i) & ~twice,
-		twice = (once & i) | (twice & ~i)
-	print once
+    once, twice = 0, 0
+        for x in arr:
+            once, twice  = (once ^ x) & ~twice , (once & x) | (twice & ~x)
+        print once
 chooseOnce([12,1,12,3,12,1,1,2,3,3])
