236051c998a95798c5ddf409efbe38a24ae47864
luckyScript <jsjhlk@163.com>
2015-11-26 16:28:55 +0800

    review week3 tut and fix some bugs

--- Haskell/anu/Week3.hs
+++ Haskell/anu/Week3.hs
@@ -1,22 +1,19 @@
 data Grades = Fail | Pass | Credit | Distinction | High_Distinction
     deriving (Show)
-data Maybe a = Nothing | Just a
-grade' :: Integer -> Main.Maybe Grades
+data MyMaybe a = MyNothing | MyJust a
+    deriving (Show)
+grade' :: Integer -> MyMaybe Grades
 grade' mark
-    | mark >= 80 && mark <= 100 = Just High_Distinction
-    | mark >= 70 && mark < 80   = Just Distinction
-    | mark >= 60 && mark < 70   = Just Credit
-    | mark >= 50 && mark < 60   = Just Pass
-    | mark >=  0 && mark < 50   = Just Fail
-    | mark <   0 || mark > 100  = Nothing
+    | mark >= 80 && mark <= 100 = MyJust High_Distinction
+    | mark >= 70 && mark < 80   = MyJust Distinction
+    | mark >= 60 && mark < 70   = MyJust Credit
+    | mark >= 50 && mark < 60   = MyJust Pass
+    | mark >=  0 && mark < 50   = MyJust Fail
+    | mark <   0 || mark > 100  = MyNothing
     | otherwise = error "Program error: Non-exhaustive guards in function: grade"
 
 
-area_of_triangle' :: Float -> Float -> Float -> Main.Maybe Float
-area_of_triangle' x y z 
-    | (x + y > z)&&(x + z > y)&&(y + z > x) = Just sqrt(heron * (heron - x) * (heron - y) * (heron -z))
-    | otherwise    = Nothing
-    where heron = (x + y + z)/2
+
 
 data Day_Names =
     Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
@@ -32,32 +29,33 @@ day_name_to_iso_day_no day = case day of
     Sunday    -> 7
 day_name_to_iso_no_guard :: Day_Names -> Integer
 day_name_to_iso_no_guard day
-    | day == "Monday"       = 1
-    | day == "Tuesday"      = 2
+    | day == Monday       = 1
+    | day == Tuesday      = 2
 
-maybe_divide :: Maybe Integer -> Maybe Integer -> Maybe Integer
---maybe_divide Maybe_x Maybe_y = case (Maybe_x,Maybe_y) of
-    (Nothing,_) -> Nothing
-    (_,Nothing) -> Nothing
-    (_,0) -> Nothing
-    (Just x, Just y) -> x `div` y
 
-data Move :: Paper | Rock | Scissors 
+mymaybe_divide :: MyMaybe Integer -> MyMaybe Integer -> MyMaybe Integer
+mymaybe_divide myMaybe_x myMaybe_y = case (myMaybe_x,myMaybe_y) of
+    (MyNothing,_) -> MyNothing
+    (_,MyNothing) -> MyNothing
+    (_,MyJust 0) -> MyNothing
+    (MyJust x, MyJust y) -> MyJust (x `div` y)
+
+data Move = Paper | Rock | Scissors 
     deriving (Eq, Show) 
 data Result = Win | Draw | Lose 
     deriving (Eq, Show)
 
 -- function returns the move which beats the other move
-beats :: Move -> Move -> Move 
+beats :: Move -> Move 
 beats move = case move of
-        paper    -> Scissors
-        rock     -> Paper
-        scissors -> Rock
+        Paper    -> Scissors
+        Rock     -> Paper
+        Scissors -> Rock
 -- find out what the score is for moves and return Win or Draw or Lose
 
 score :: Move -> Move -> Result
 score this_move opposing_move = case this_move of
-        opposing_move -> Draw
-        _
-               | this_move == beats opposing_move -> Win
-               | _                               -> Lose
+    opposing_move -> Draw
+    _
+        | this_move == beats opposing_move -> Win
+        | otherwise                        -> Lose
