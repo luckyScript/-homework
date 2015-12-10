578f606b6842eb3facfcb23c68abbbe64f8dfabf
luckyScript <jsjhlk@163.com>
2015-12-10 13:25:16 +0800

    remove c

--- Algorithm/算法竞赛入门经典/part1/chapter1/1-1.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int
-main(void)
-{
-    int a, b, c;
-    scanf("%d%d%d",&a,&b,&c);
-    double d = (double)(a+b+c);
-    printf("%.3lf",d/3.0);
-}
--- Algorithm/算法竞赛入门经典/part1/chapter1/1-7.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int
-main(void)
-{
-    int year;
-    scanf("%d",&year);
-    (year%4==0&&year%100!=0)||year%400==0 ? printf("yes"):printf("no");
-    return 0;
-}
